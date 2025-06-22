337fe39e4cd240a99f79da294f888dfc01a7efd8
simhanik <nikhilsahu0427@gmail.com>
2025-06-22 23:12:48 +0530

    added airport seeders

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const CityRepository =require('./src/repository/city-repository.js')
 
 const ApiRoutes = require('./src/routes/index.js')
 
+// this is require for the model synchronization
+const db = require('./src/models/index.js')
+const {Airport,City} = require('./src/models/index.js')
+
 const setupAndStartServer = async()=> {
     // create an express object
     const app = express()
@@ -16,12 +20,16 @@ const setupAndStartServer = async()=> {
 
     app.use('/api',ApiRoutes)
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async() => {
         console.log(`Sever started on port  : ${PORT}`)
 
-        // const repo = new CityRepository()
-        // repo.createCity({name:"New Delhi"})
-        
+        // this is for the model synchronization
+        // whenever you want to synchronize add SYNC_DB in your environment variables
+        // as we need to do sync one after model creation
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter:true})
+        }
+
     })
 }
 setupAndStartServer()
